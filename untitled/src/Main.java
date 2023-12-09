c641c892f5af876e1eb2b638a6eef1c498b7fdd1
Hyder Ali <hyderrali123@gmail.com>
2023-12-09 16:16:39 -0500

    Java File

---
+++ untitled/src/Main.java
@@ -0,0 +1,17 @@
+// Hyder Ali//
+// C0893786 //
+
+import java.util.ArrayList;
+import java.util.List;
+
+class SingleObject {
+    private String name;
+
+    public SingleObject(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
