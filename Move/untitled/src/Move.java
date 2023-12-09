bba011b7647b5686d57cf0ca0d56571d8d75cf15
Hyder Ali <hyderrali123@gmail.com>
2023-12-09 16:04:12 -0500

    SingleObject

--- Move/untitled/src/Move.java
+++ Move/untitled/src/Move.java
@@ -4,6 +4,17 @@
 import java.util.ArrayList;
 import java.util.List;
 
+class SingleObject {
+    private String name;
+
+    public SingleObject(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
 class Move {
     private List<Box> boxes = new ArrayList<>();
 
